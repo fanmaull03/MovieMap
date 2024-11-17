a6d42d7d56b0fbae0f5d3b1a4632ac9c986abb72
fanmaull03 <ifanmaul03@gmail.com>
2024-11-17 20:22:23 +0700

    udah login & sign up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,27 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    redirectTo: 'login',  // Mengarahkan ke halaman login
+    pathMatch: 'full'     // Pastikan cocok dengan path kosong sepenuhnya
   },
   {
-    path: 'movie-detail/:id',  // Menambahkan parameter ID untuk film
+    path: 'movie-detail/:id',
     loadChildren: () => import('./movie-detail/movie-detail.module').then(m => m.MovieDetailPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
+  },
+  {
+    path: 'signup',
+    loadChildren: () => import('./signup/signup.module').then(m => m.SignupPageModule)
+  },
+  {
+    path: 'tabs',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   }
 ];
+
 
 @NgModule({
   imports: [
