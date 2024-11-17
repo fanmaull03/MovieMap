d6b43ead8c151820345a9b96e7b000c493ce7980
Rifqifauu <120528673+Rifqifauu@users.noreply.github.com>
2024-11-17 21:20:40 +0700

    Membuat tombol login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'login',  // Mengarahkan ke halaman login
-    pathMatch: 'full'     // Pastikan cocok dengan path kosong sepenuhnya
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
   {
     path: 'movie-detail/:id',
