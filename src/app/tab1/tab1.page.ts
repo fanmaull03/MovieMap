50cd8086236fd9382824c84f6f2af8294c25cd90
Rifqifauu <120528673+Rifqifauu@users.noreply.github.com>
2024-11-17 19:11:14 +0700

    Membuat halaman search

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { OmdbService } from '../services/omdb.service';
 
 @Component({
   selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  templateUrl: './tab1.page.html',
+  styleUrls: ['./tab1.page.scss'],
 })
 export class Tab1Page {
+  query: string = '';
+  movies: any[] = [];
+  error: string = '';
 
-  constructor() {}
+  constructor(private omdbService: OmdbService) {}
 
+  searchMovies() {
+    if (this.query.trim() === '') {
+      this.error = 'Please enter a search term!';
+      return;
+    }
+
+    this.omdbService.searchMovies(this.query).subscribe(
+      (response) => {
+        if (response.Response === 'True') {
+          this.movies = response.Search;
+          this.error = '';
+        } else {
+          this.error = response.Error;
+          this.movies = [];
+        }
+      },
+      (error) => {
+        this.error = 'An error occurred while fetching movies.';
+        console.error(error);
+      }
+    );
+  }
 }
