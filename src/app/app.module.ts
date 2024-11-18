7d9071494d1aa2942dbef332f75697f70812722e
fanmaull03 <ifanmaul03@gmail.com>
2024-11-18 15:49:17 +0700

    login & signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { provideHttpClient } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(),
-    AppRoutingModule,
-    FormsModule
-  ],
-  providers: [
-    provideHttpClient(), // Menggunakan provideHttpClient
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
-  ],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideHttpClient()],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
