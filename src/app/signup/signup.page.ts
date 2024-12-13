7748975aaac5ceba26ae4e3b968d936de8cda00c
fanmaull03 <ifanmaul03@gmail.com>
2024-12-13 10:43:34 +0700

    signup

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.page.html',
@@ -10,16 +11,24 @@ export class SignupPage {
   formData = {
     name: '',
     email: '',
-    password: ''
+    password: '',
+    confirmPassword: ''
   };
 
   constructor(private router: Router, private authService: AuthService) {}
 
   register() {
+    // Validasi apakah password dan confirmPassword cocok
+    if (this.formData.password !== this.formData.confirmPassword) {
+      alert('Password and Confirm Password do not match!');
+      return;
+    }
+
+    // Panggil API untuk menyimpan data
     this.authService.register(this.formData).subscribe(
       (response: any) => {
         alert('Registration successful!');
-        this.router.navigate(['/login']);   
+        this.router.navigate(['/login']); // Pindah ke halaman login
       },
       (error: any) => {
         console.error('Registration failed:', error);
